e802dfe3a5540ca731a9660c022d5c52c59e36f9
Andrew Shumeiko <shumeiko.drew@gmail.com>
2022-01-10 00:05:08 +0500

    complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const posNum = n < 0 ? n * -1 : n;
+    const numberOfString = posNum.toString();
+    const stringOfArray = numberOfString.split("");
+    return +stringOfArray.reverse().join("");
+};
+// function reverse(n) {
+//     const posNum = n < 0 ? n * -1 : n;
+//     const numberOfString = posNum.toString();
+//     const stringOfArray = numberOfString.split("");
+//     return +stringOfArray.reverse().join("");
+// }
+
+// console.log(reverse(-3124124));
